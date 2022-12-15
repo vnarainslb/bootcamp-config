144409f0790d6e20d4b085a731e2eaff817b82d9
vnarainslb <vnarain@slb.com>
2022-12-15 05:38:52 +0000

    vaibhav

--- config/transformations/create_asset_hierarchy/transformation.sql
+++ config/transformations/create_asset_hierarchy/transformation.sql
@@ -1,5 +1,16 @@
-  SELECT ts.externalId AS externalId,
-         ast.id AS assetId
-  FROM `_cdf`.`timeseries` AS ts
-
-  INNER JOIN `_cdf`.`assets` AS ast ON split(ts.externalId, ":") [0] = ast.externalId
+  SELECT
+    external_id AS externalId,
+    IF(
+      parent_external_id = ''
+      OR parent_external_id IS NULL,
+      '',
+      parent_external_id
+    ) AS parentExternalId,
+    name AS name,
+    json_tuple(metadata,'site','type', 'cycle time', 'Function', 'src', 'Machine type') AS (site, type, cycle_time, function, src, machine_type),
+    to_metadata(site,type, cycle_time, function, src, machine_type) AS metadata,
+    description AS description,
+    'from RAW' AS source,
+    Dataset_id('src:001:sap:ds') AS dataSetId
+  FROM
+    `src:001:sap:db`.assets
